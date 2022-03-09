9139ea0d2a0908cbd8194c9c2c6145f162b46cdb
Malik8008 <tu60prbs1@code.edu.az>
2022-03-09 12:00:37 +0400

    Divide proccess added

---
+++ Main Task-1/Main Task-1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Main_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int a = number % 7;
+            //int b = 7 - a;
+            //if (number%7==0)
+            //{
+            //    Console.WriteLine("7-e bolunur");
+            //}
+            //else if (a>b)
+            //{
+            //    Console.WriteLine(number+b);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(number-a);
+            //}
+
+
+            Console.WriteLine(Divide(26));
+        }
+
+        public static int Divide(int num)
+        {
+
+            if (num % 7 == 0)
+            {
+                Console.WriteLine("7-e bolunur");
+            }
+            int a = num % 7;
+            int b = 7 - a;
+            if (a > b)
+            {
+                return num + b;
+            }
+            else
+            {
+                return num - a;
+            }
+        }
+    }
+}
