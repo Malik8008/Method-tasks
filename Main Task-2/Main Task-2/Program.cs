9b7c0ea4177977295c2edfefb8e4eda7f62a41c7
Malik8008 <tu60prbs1@code.edu.az>
2022-03-09 12:01:19 +0400

    Operator proccess added

---
+++ Main Task-2/Main Task-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Main_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Operator(10,5));
+        }
+
+        public static int Operator(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Operator(int num1, int num2, string Sub)
+        {
+            return num1 - num2;
+        }
+        public static int Operator(int num1, int num2, int num3, string Div)
+        {
+            return num1 / num2;
+        }
+        public static string Operator(int num1, int num2, int num3, int num4, string Multi)
+        {
+            return num1 * num2 + " " + Multi;
+        }
+    }
+}
